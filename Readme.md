546d2b1b71e57bb33bb626bf034f550cf07d710e
Steel64 <updown1.jp@gmail.com>
2025-06-27 17:51:18 -0500

    endpoints_noticias_listos

---
+++ Readme.md
@@ -0,0 +1,32 @@
+# Documento de Endpoints GameStore-backend
+
+## Seccion de noticias
+
+### Endpoint: Listar noticias
+
+- Path: "/noticias"
+- Metodo: GET
+- Input: 
+
+- Output
+
+```json
+[
+    {
+        "id" : 1,
+        "title" : "titulo",
+        "categoria" : "accion",
+        "autor" : "nintendo",
+        "redaccion" : "Los videojuegos ...",
+        "image" : "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png",
+        "dias" : 100,
+        "select" : false
+    }, {}, {}, ...
+]
+```
+
+### Endpoint: Registrar una noticia
+
+### Endpoint: Modifcar una noticia
+
+### Endpoint: Eliminar una noticia
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from "express"
 import dotenv from "dotenv"
-import { listaNoticias } from "./noticias"
+import { listaNoticias, noticia } from "./noticias"
 import bodyParser from "body-parser"
 import cors from "cors"
 
@@ -25,36 +25,174 @@ app.get("/noticias",(req : Request , resp : Response) => {
 })
 
 app.post("/noticias",(req : Request , resp : Response) =>{
-    const noticia = req.body
+    const nota = req.body
     const listaNot = listaNoticias
-    
-    if(noticia.id == undefined ||
-        noticia.title == undefined ||
-        noticia.categoria == undefined ||
-        noticia.autor == undefined ||
-        noticia.redaccion == undefined ||
-        noticia.image == undefined ||
-        noticia.dias == undefined ||
-        noticia.select == undefined)
-        {
-            resp.status(400).json({
-                msg : "Debe completar los campos vacios"
-            })
-            return
-        }
-    
+
+    if(nota.id == undefined)
+    {
+        resp.status(400).json({
+            msg : "Debe llenar el nuevo id"
+        })
+        return
+    }
+    if(nota.title == undefined)
+    {
+        resp.status(400).json({
+            msg : "Debe llenar el nuevo titulo"
+        })
+        return
+    }
+    if(nota.categoria == undefined)
+    {
+        resp.status(400).json({
+            msg : "Debe llenar la nueva categoria"
+        })
+        return
+    }
+    if(nota.autor == undefined)
+    {
+        resp.status(400).json({
+            msg : "Debe llenar el nuevo autor"
+        })
+        return
+    }
+    if(nota.redaccion == undefined)
+    {
+        resp.status(400).json({
+            msg : "Debe llenar la nueva redaccion"
+        })
+        return
+    }
+    if(nota.image == undefined)
+    {
+        resp.status(400).json({
+            msg : "Debe llenar el nuevo link de la imagen"
+        })
+        return
+    }
+    if(nota.dias == undefined)
+    {
+        resp.status(400).json({
+            msg : "Debe llenar el nuevo link de la imagen"
+        })
+        return
+    }
+    if(nota.select == undefined)
+    {
+        resp.status(400).json({
+            msg : "Debe llenar el nuevo link de la imagen"
+        })
+        return
+    }
+
     listaNot.push({
-        id : new Date().getTime(),
-        title : noticia.title,
-        categoria : noticia.categoria,
-        autor : noticia.autor,
-        redaccion: noticia.redaccion,
-        image : noticia.image,
-        dias : noticia.dias,
-        select : noticia.select
+        id : nota.id,
+        title : nota.title,
+        categoria : nota.categoria,
+        autor : nota.autor,
+        redaccion: nota.redaccion,
+        image : nota.image,
+        dias : nota.dias,
+        select : nota.select
     })
     resp.json({
         msg: ""
+    })
+})
+
+app.put("/noticias/:id",(req : Request , resp : Response) => {
+    const nota = req.body
+    const notaId = req.params.id
+    const listaNot = listaNoticias
+
+    if(notaId == undefined)
+    {
+        resp.status(400).json({
+            msg : "Debe enviar un id como parte del path"
+        })
+        return
+    }
+
+    if(nota.title == undefined)
+    {
+        resp.status(400).json({
+            msg : "Debe enviar el titulo"
+        })
+        return
+    }
+    if(nota.categoria == undefined)
+    {
+        resp.status(400).json({
+            msg : "Debe enviar la categoria"
+        })
+        return
+    }
+    if(nota.autor == undefined)
+    {
+        resp.status(400).json({
+            msg : "Debe enviar el autor"
+        })
+        return
+    }
+    if(nota.redaccion == undefined)
+    {
+        resp.status(400).json({
+            msg : "Debe enviar la redaccion"
+        })
+        return
+    }
+    if(nota.image == undefined)
+    {
+        resp.status(400).json({
+            msg : "Debe enviar el link de la imagen"
+        })
+        return
+    }
+
+    for (let nt of listaNot)
+    {
+        if(nt.id.toString() == notaId){
+            nt.title = nota.descripcion
+            nt.categoria = nota.categoria
+            nt.autor = nota.autor
+            nt.redaccion = nota.redaccion
+            nt.image = nota.image
+            resp.json({
+                msg : ""
+            })
+            return
+        }
+        resp.status(400).json({
+            msg : "No existe noticia con ese id"
+        })
+        return
+    }
+
+    resp.status(400).json({
+        msg : "No existe todo con ese id"
+    })
+    return
+})
+
+app.delete("/noticias/:id",(req : Request , resp : Response) => {
+    const notaId = req.params.id
+    const lista = listaNoticias
+
+    const indiceAEliminar = lista.findIndex((nt : noticia) => {
+        return nt.id.toString() == notaId
+    })
+
+    if(indiceAEliminar == -1){
+        resp.status(400).json({
+            msg : "No existe noticia con ese id"
+        })
+        return
+    }
+
+    lista.splice(indiceAEliminar, 1)
+
+    resp.json({
+        msg : ""
     })
 })
 
